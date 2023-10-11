3863cf0c22db5b9e052e699419b22bfe1e27c505
Hang Jung Ling <hang-jung.ling@insa-lyon.fr>
2023-10-11 15:20:11 +0200

    Add `size` argument in `resize_camus`

--- src/dataset_conversion/resize_camus.py
+++ src/dataset_conversion/resize_camus.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 from pathlib import Path
 from typing import Union
 
@@ -10,12 +11,17 @@ from src.utils.file_and_folder_operations import subdirs, subfiles
 from src.utils.transforms import resample_image, resample_label
 
 
-def resize_dataset(data_dir: Union[Path, str]) -> None:
+def resize_dataset(data_dir: Union[Path, str], size: int = 64) -> None:
     """Resize CAMUS dataset to 64x64.
 
     Args:
         data_dir: Path to the dataset.
+        size: Size to resample the dataset.
     """
+    if not isinstance(size, int):
+        size = int(size)
+        warnings.warn(f"Size provided is not an integer, rounding to {size}.")
+
     all_cases = subdirs(data_dir, prefix="patient", join=False)
 
     for case in tqdm(all_cases, desc="Resizing CAMUS dataset", unit="patients"):
@@ -24,7 +30,7 @@ def resize_dataset(data_dir: Union[Path, str]) -> None:
             image = sitk.ReadImage(file)
             ori_shape = [*image.GetSize(), 1]
             ori_spacing = [*image.GetSpacing(), 1]
-            new_shape = [64, 64, ori_shape[-1]]
+            new_shape = [size, size, ori_shape[-1]]
             new_spacing = np.array(ori_spacing) * np.array(ori_shape) / np.array(new_shape)
 
             image_array = sitk.GetArrayFromImage(image).transpose(1, 0)
@@ -46,7 +52,7 @@ def main():
     import argparse
 
     PATH_DATA = Path(__file__).parent.parent.parent / "data" / "camus"
-    parser = argparse.ArgumentParser(description="Resizing CAMUS dataset to 64x64.")
+    parser = argparse.ArgumentParser(description="Resizing the CAMUS dataset.")
     parser.add_argument(
         "-d",
         "--data_dir",
@@ -54,8 +60,15 @@ def main():
         default=PATH_DATA,
         help="Path to the original CAMUS dataset.",
     )
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=int,
+        default=64,
+        help="Size to resample the CAMUS dataset: (size, size).",
+    )
     args = parser.parse_args()
-    resize_dataset(args.data_dir)
+    resize_dataset(args.data_dir, args.size)
 
 
 if __name__ == "__main__":
